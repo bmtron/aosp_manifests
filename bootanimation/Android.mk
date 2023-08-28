7d83f61aaf29b51323d362a7d0a258b6aa77c5d6
Brendan <bmmeehan3@gmail.com>
2023-08-28 19:36:41 -0400

    custom boot animation

---
+++ bootanimation/Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+TARGET_BOOTANIMATION := vendor/bmtron/bootanimation/bootanimation.zip
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := bootanimation.zip
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT)/media
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): $(TARGET_BOOTANIMATION)
+    @mkdir -p $(dir $@)
+    @cp $(TARGET_BOOTANIMATION) $@
