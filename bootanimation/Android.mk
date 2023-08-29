49893c1e3effb2abd29e76a9107bea086b5753de
Brendan <bmmeehan3@gmail.com>
2023-08-29 00:06:01 -0400

    fixed mk file

--- bootanimation/Android.mk
+++ bootanimation/Android.mk
@@ -1,14 +1,14 @@
-LOCAL_PATH := $(call my-dir)
+#LOCAL_PATH := $(call my-dir)
 
-TARGET_BOOTANIMATION := vendor/bmtron/bootanimation/bootanimation.zip
+#TARGET_BOOTANIMATION := vendor/bmtron/bootanimation/bootanimation.zip
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := bootanimation.zip
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/media
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := bootanimation.zip
+#LOCAL_MODULE_CLASS := ETC
+#LOCAL_MODULE_PATH := /system/media
 
-include $(BUILD_SYSTEM)/base_rules.mk
+#include $(BUILD_SYSTEM)/base_rules.mk
 
-$(LOCAL_BUILT_MODULE): $(TARGET_BOOTANIMATION)
-	@mkdir -p $(dir $@)
-	@cp $(TARGET_BOOTANIMATION) $@
+#$(LOCAL_BUILT_MODULE): $(TARGET_BOOTANIMATION)
+#	@mkdir -p $(dir $@)
+#	@cp $(TARGET_BOOTANIMATION) $@
